d8be36ef83a63fd2724dbf9bf2728e7b2be54d03
Jacky Shen <mebusw@163.com>
2014-08-12 16:25:10 +0800

    Update _element.py

--- src/AppiumLibrary/keywords/_element.py
+++ src/AppiumLibrary/keywords/_element.py
@@ -28,13 +28,13 @@ class _ElementKeywords(KeywordGroup):
             class_name = self._get_class(_platform_class_dict)
             self._click_element_by_class_name(class_name, index_or_name)
 
-    def click_table_cell(self, index_or_name):
-        """ Click button """
+    def click_table_cell(self, index):
+        """ Click table cell """
         _platform_class_dict = {'ios':'UIATableCell',
                                    'android': '???'}
         if self._is_support_platform(_platform_class_dict):
-            class_name = self._get_class(_platform_class_dict)
-            self._click_element_by_class_name(class_name, index_or_name)
+            row = self._find_elements_by_class_name("UIATableCell")[2]
+            row.click()
 
     def input_text(self, locator, text):
         """Types the given `text` into text field identified by `locator`.
